645fcbcfe7df52425e80460200657041c0ae0daf
Ronney Vargas <ronneyvargas@gmail.com>
2019-02-09 17:35:57 -0600

    First commit.

---
+++ script.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+var jukeBox = document.querySelector('ul.player');
+
+jukeBox.addEventListener('click', function(e) {
+    var songName = e.target.getAttribute('data-src');
+    var songPlaying = document.querySelector('#player');
+
+    if (songPlaying) {
+        if (songPlaying.paused) {
+            songPlaying.play();
+            e.target.id = 'playing';
+        } else {
+            songPlaying.pause();
+            e.target.id = 'paused';
+        }
+
+    } else {
+        var audioPlayer = document.createElement('audio');
+        audioPlayer.id = 'player';
+        e.target.id = 'playing';
+        audioPlayer.src = songName;
+        document.body.appendChild(audioPlayer);
+        audioPlayer.play();
+
+        // will remove the audio from DOM once done playing
+        audioPlayer.addEventListener('ended', function () {
+            audioPlayer.parentNode.removeChild(audioPlayer);
+            e.target.id = '';
+        }, false);
+    }
+
+}, false);
